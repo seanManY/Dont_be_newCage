ae2fdc16a38d71d01b7e8df8ae2cef3b900f3b99
Sean Maguire <maguire.sean037@gmail.com>
2018-10-03 15:33:11 -0700

    update this

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    Vector2 spawnPosition;
+    GameObject floatingText;
+
+    RectTransform rectTransform = GetComponent<RectTransform>().anchoredPosition;
+
+    // Use this for initialization
+    void Start () {
+
+        
+
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        spawnPosition = new Vector2(this.GetComponent<RectTransform>().anchoredPosition.x-1,
+                                    this.GetComponent<RectTransform>().anchoredPosition.y);
+        floatingText.GetComponent<RectTransform>().anchoredPosition = spawnPosition;
+    }
+}
