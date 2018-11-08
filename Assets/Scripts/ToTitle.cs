34f98c82e37f51257a4cba5b176459c95ff5d391
Sean Maguire <maguire.sean037@gmail.com>
2018-11-07 16:16:26 -0800

    Is this Done?

---
+++ Assets/Scripts/ToTitle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ToTitle : MonoBehaviour {
+
+    public float delay = 440;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+	
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(0);
+    }
+}
