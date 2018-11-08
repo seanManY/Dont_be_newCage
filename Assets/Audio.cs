9b6128c9a03cb90595d553279f82645d92d32cb9
Sean Maguire <maguire.sean037@gmail.com>
2018-11-07 16:57:38 -0800

    now thats what i call music

---
+++ Assets/Audio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Audio : MonoBehaviour {
+
+    private void Awake()
+    {
+        
+         DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+            
+
+    }
+}
